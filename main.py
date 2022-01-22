9c9eabdf67604d5b162f603d720145c63ac84660
Samasaur1 <30577766+Samasaur1@users.noreply.github.com>
2022-01-22 12:02:54 -0800

    Add the ability to fill out the form
    Note that this assumes clicking the next button goes to the next page successfully
    I haven't actaully tested it on a live form yet

---
+++ main.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+def handlePage1(driver):
+    consent = WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.ID, "QR~QID11~1"))
+    #consent = driver.find_element(By.ID, "QR~QID11~1")
+    consent.click()
+
+    nextButton = WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.ID, "NextButton"))
+    nextButton.click()
+
+def handlePage2(driver):
+    WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID, "QR~QID84"))
+
+    nextButton = WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.ID, "NextButton"))
+    nextButton.click()
+
+def handlePage3(driver):
+    none = WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.ID, "QR~QID47~7"))
+    none.click()
+
+    nextButton = WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.ID, "NextButton"))
+    nextButton.click()
+
+def handlePage4(driver):
+    none = WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.ID, "QR~QID46~14"))
+    none.click()
+
+    nextButton = WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.ID, "NextButton"))
+    nextButton.click()
+
+def fillOutCheckAtURLWithDriver(url, driver):
+    driver.get(url)
+    handlePage1(driver)
+    handlePage2(driver)
+    handlePage3(driver)
+    handlePage4(driver)
+
+def fillOutCheckAtURL(url):
+    driver = webdriver.Firefox()
+    fillOutCheckAtURLWithDriver(url, driver)
+    driver.quit()
